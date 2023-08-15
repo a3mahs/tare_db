93dc40e5c6bf4984e134d1d4f391b9740dbc4f05
miguellangelhs <miguellangelhs@gmail.com>
2023-08-14 23:03:27 -0500

    Editar Full

--- resources/views/propiedads/edit.blade.php
+++ resources/views/propiedads/edit.blade.php
@@ -22,95 +22,148 @@
                                         <div class="row">
                                             {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
                                             <div class="col-sm">
-                                                <input type="text" class="form-control" name="categoria"
-                                                    placeholder="Categoria" value="{{ old('categoria', $propiedad->categoria) }}" autocomplete="off" autofocus>
+                                                <select class="form-control" name="categoria" required>
+                                                    <option value="0">Seleccione categoria</option>
+                                                    @foreach ($inmuebles as $inmueble)
+                                                        @if ($propiedad->categoria == $inmueble['id'])
+                                                            <option value="{{ $inmueble['id'] }}" selected>
+                                                                {{ $inmueble['categoria_inmueble'] }}</option>
+                                                        @else
+                                                            <option value="{{ $inmueble['id'] }}">
+                                                                {{ $inmueble['categoria_inmueble'] }}</option>
+                                                        @endif
+                                                    @endforeach
+                                                </select>
                                             </div>
                                             <div class="col-sm">
                                                 <input type="text" class="form-control" name="construccion"
-                                                    placeholder="Año" value="{{ old('construccion', $propiedad->construccion) }}" autocomplete="off" autofocus>
+                                                    placeholder="Año"
+                                                    value="{{ old('construccion', $propiedad->construccion) }}"
+                                                    autocomplete="off" autofocus>
                                             </div>
                                             <div class="col-sm">
-                                                <input type="text" class="form-control" name="estado"
-                                                    placeholder="Estado" value="{{ old('estado', $propiedad->estado) }}" autocomplete="off" autofocus>
+                                                <select class="form-control" name="estado" required>
+                                                    <option value="0">Seleccione el estado</option>
+                                                    @foreach ($estados as $estado)
+                                                        @if ($propiedad->estado == $estado['id'])
+                                                            <option value="{{ $estado['id'] }}" selected>
+                                                                {{ $estado['estado_propiedad'] }}</option>
+                                                        @else
+                                                            <option value="{{ $estado['id'] }}">
+                                                                {{ $estado['estado_propiedad'] }}</option>
+                                                        @endif
+                                                    @endforeach
+                                                </select>
                                             </div>
                                         </div>
 
                                         <div class="row">
-                                          {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="direccion"
-                                                  placeholder="Dirección" value="{{ old('direccion', $propiedad->direccion) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                            <input type="text" class="form-control" name="barrio"
-                                                placeholder="Barrio/Zona" value="{{ old('barrio', $propiedad->barrio) }}" autocomplete="off" autofocus>
+                                            {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="direccion"
+                                                    placeholder="Dirección"
+                                                    value="{{ old('direccion', $propiedad->direccion) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="barrio"
+                                                    placeholder="Barrio/Zona"
+                                                    value="{{ old('barrio', $propiedad->barrio) }}" autocomplete="off"
+                                                    autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="estrato"
+                                                    placeholder="Estrato" value="{{ old('estrato', $propiedad->estrato) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
                                         </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="estrato"
-                                                  placeholder="Estrato" value="{{ old('estrato', $propiedad->estrato) }}" autocomplete="off" autofocus>
-                                          </div> 
-                                      </div>
 
-                                      <div class="row">
-                                          {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="ciudad"
-                                                  placeholder="Ciudad" value="{{ old('ciudad', $propiedad->ciudad) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="valor"
-                                                  placeholder="Valor" value="{{ old('valor', $propiedad->valor) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="contactos"
-                                                  placeholder="Contactos" value="{{ old('contactos', $propiedad->contactos) }}" autocomplete="off" autofocus>
-                                          </div>
-                                      </div>
+                                        <div class="row">
+                                            {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="ciudad"
+                                                    placeholder="Ciudad" value="{{ old('ciudad', $propiedad->ciudad) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="valor"
+                                                    placeholder="Valor" value="{{ old('valor', $propiedad->valor) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="contactos"
+                                                    placeholder="Contactos"
+                                                    value="{{ old('contactos', $propiedad->contactos) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                        </div>
 
-                                      <div class="row">
-                                          {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="propietario"
-                                                  placeholder="Propietario" value="{{ old('propietario', $propiedad->propietario) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="tipo_documento"
-                                                  placeholder="Tipo documento" value="{{ old('tipo_documento', $propiedad->tipo_documento) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="documento"
-                                                  placeholder="Número documento" value="{{ old('documento', $propiedad->documento) }}" autocomplete="off" autofocus>
-                                          </div>
-                                      </div>
+                                        <div class="row">
+                                            {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="propietario"
+                                                    placeholder="Propietario"
+                                                    value="{{ old('propietario', $propiedad->propietario) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <select class="form-control" name="tipo_documento" required>
+                                                    <option value="0">Seleccione el estado</option>
+                                                    @foreach ($documentos as $documento)
+                                                        @if ($propiedad->tipo_documento == $documento['id'])
+                                                            <option value="{{ $documento['id'] }}" selected>
+                                                                {{ $documento['documento_per'] }}</option>
+                                                        @else
+                                                            <option value="{{ $documento['id'] }}">
+                                                                {{ $documento['documento_per'] }}</option>
+                                                        @endif
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="documento"
+                                                    placeholder="Número"
+                                                    value="{{ old('documento', $propiedad->documento) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                        </div>
 
-                                      <div class="row">
-                                          {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="contactos_propietario"
-                                                  placeholder="Contacto de propietario" value="{{ old('contactos_propietario', $propiedad->contactos_propietario) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="direccion_propietario"
-                                                  placeholder="Dirección de propietario" value="{{ old('direccion_propietario', $propiedad->direccion_propietario) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="correo"
-                                                  placeholder="Correo" value="{{ old('correo', $propiedad->correo) }}" autocomplete="off" autofocus>
-                                          </div>
-                                      </div>
+                                        <div class="row">
+                                            {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="contactos_propietario"
+                                                    placeholder="Contacto de propietario"
+                                                    value="{{ old('contactos_propietario', $propiedad->contactos_propietario) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="direccion_propietario"
+                                                    placeholder="Dirección de propietario"
+                                                    value="{{ old('direccion_propietario', $propiedad->direccion_propietario) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="correo"
+                                                    placeholder="Correo" value="{{ old('correo', $propiedad->correo) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                        </div>
 
-                                      <div class="row">
-                                          {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="nota"
-                                                  placeholder="Nota" value="{{ old('nota', $propiedad->nota) }}" autocomplete="off" autofocus>
-                                          </div>
-                                          <div class="col-sm">
-                                              <input type="text" class="form-control" name="observacion"
-                                                  placeholder="Observación" value="{{ old('observacion', $propiedad->observacion) }}" autocomplete="off" autofocus>
-                                          </div>
+                                        <div class="row">
+                                            {{-- <label for="title" class="col-sm-2 col-form-label">Post title</label> --}}
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="nota"
+                                                    placeholder="Nota" value="{{ old('nota', $propiedad->nota) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
+                                            <div class="col-sm">
+                                                <input type="text" class="form-control" name="observacion"
+                                                    placeholder="Observación"
+                                                    value="{{ old('observacion', $propiedad->observacion) }}"
+                                                    autocomplete="off" autofocus>
+                                            </div>
 
-                                      </div>
+                                        </div>
                                         {{-- <div class="col-sm-7">
                                             <input type="text" class="form-control" name="title"
                                                 placeholder="Ingrese el title" value="{{ old('title', $propiedad->title) }}"
@@ -122,7 +175,8 @@
                             <!--End body-->
                             <!--Footer-->
                             <div class="card-footer ml-auto mr-auto">
-                                <a href="{{ route('propiedads.index') }}" class="btn btn-sm btn-success mr-3"> Volver </a>
+                                <a href="{{ route('propiedads.index') }}" class="btn btn-sm btn-success mr-3"> Volver
+                                </a>
                                 <button type="submit" class="btn btn-primary">Guardar</button>
                             </div>
                         </div>
